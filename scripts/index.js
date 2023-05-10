9746b7e0a0234e707e74c515f808c95a27b458c8
Usman <usman.mughal@outlook.fr>
2023-05-10 16:48:33 +0200

    màj des tags

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ import { recipeFactory } from './factory/recipeFactory.js'
 import { filterFactory } from './factory/filterFactory.js'
 import { filterRecipes, sortRecipes } from './utils/filterAlgo.js'
 import { toggleDropDown } from './utils/dropdown.js'
-import { createTag } from './utils/tag.js'
+// import { filterByTag } from './utils/tag.js'
 
 // déclaration variables
 let recipes = []
@@ -12,6 +12,7 @@ let appliancesList = []
 let ustensilsList = []
 let lists = []
 let filteredRecipes = []
+let searchBarKeyword = []
 
 // création et affichage des cards recette via la recipeFactory
 function displayRecipes(recipes) {
@@ -97,7 +98,7 @@ function displayFilterList(lists, keyword = null) {
 			// on postionne un eventlistener sur chaque option de filtre li
 			Object.values(filterListCardDOM).forEach((li) => {
 				li.addEventListener('click', (e) => {
-					createTag(e, filterName)
+					createTag(e, filterName, recipes)
 				})
 				ulSection.appendChild(li)
 			})
@@ -121,7 +122,7 @@ function displayFilterList(lists, keyword = null) {
 			// on postionne un eventlistener sur chaque option de filtre li
 			Object.values(filterListCardDOM).forEach((li) => {
 				li.addEventListener('click', (e) => {
-					createTag(e, filterName)
+					createTag(e, filterName, recipes)
 				})
 				ulSection.appendChild(li)
 			})
@@ -149,10 +150,10 @@ const searchBar = document.querySelector('#search_recipe')
 const recipesSection = document.getElementById('recipes')
 const cross = document.querySelector('.search_btn_cross')
 const searchBarForm = document.querySelector('form')
+const recipesLength = document.querySelector('.search_recipe_number')
 
 searchBar.addEventListener('input', (e) => {
 	const value = e.target.value
-	const recipesLength = document.querySelector('.search_recipe_number')
 
 	if (value.length >= 3) {
 		initResetSearchbar()
@@ -167,6 +168,9 @@ searchBar.addEventListener('input', (e) => {
 		listInit(filteredRecipes)
 		// affiche les listes des recettes filtrées
 		displayFilterList(lists, value)
+		// stock recherche saisie
+		searchBarKeyword = []
+		searchBarKeyword.push(value)
 
 		if (filteredRecipes.length === 0) {
 			recipesSection.innerHTML = "Votre recherche n'a pas de correspondance."
@@ -180,6 +184,8 @@ searchBar.addEventListener('input', (e) => {
 		recipesSection.classList.remove('empty')
 		cross.style.display = 'none'
 		document.querySelector('.search_recipe_number').textContent = '50'
+		// reset recherche saisie
+		searchBarKeyword = []
 	}
 })
 
@@ -204,10 +210,11 @@ const inputsFilter = document.querySelectorAll('.filter_input')
 inputsFilter.forEach((input) =>
 	input.addEventListener('input', (e) => {
 		const filterValue = e.target.value
+		const filterProperty = e.target.dataset.property
 		if (filterValue.length >= 3) {
-			initResetInput(e.target.dataset.property)
+			initResetInput(filterProperty)
 			// récupère les li existants
-			const ulSection = document.getElementById(`filter_list_${e.target.dataset.property}`)
+			const ulSection = document.getElementById(`filter_list_${filterProperty}`)
 			const liSectionList = ulSection.childNodes
 			const keywordFormated = filterValue
 				.toLowerCase()
@@ -233,7 +240,7 @@ inputsFilter.forEach((input) =>
 				liFilter.textContent = eltFormated
 				// on postionne un eventlistener sur chaque option de filtre li
 				liFilter.addEventListener('click', (e) => {
-					createTag(e, e.target.dataset.property)
+					createTag(e, filterProperty, recipes)
 				})
 				ulSection.appendChild(liFilter)
 			})
@@ -271,4 +278,96 @@ function initResetInput(e) {
 			init()
 		}
 	})
+}
+
+//------------------------------------------------------------------------------------------
+// Gestion des tags
+let tagList = []
+let globalKeyword = []
+let filteredRecipesByTag = []
+
+function createTag(e, selectedFilter) {
+	const filterTag = document.getElementById('filters_tags')
+	filterTag.classList.add('filters_tags_active')
+
+	const tagDiv = document.createElement('div')
+	tagDiv.classList.add('filter_tag_div')
+	tagDiv.classList.add(`color_tag_${selectedFilter}`)
+	filterTag.appendChild(tagDiv)
+
+	const tag = document.createElement('p')
+	tag.classList.add('filter_tag_p')
+	tag.textContent = e.target.innerText
+	tagDiv.appendChild(tag)
+
+	const tagIcon = document.createElement('i')
+	tagIcon.classList.add('far', 'fa-times-circle')
+	tagIcon.addEventListener('click', (e) => deleteTag(e))
+	tagDiv.appendChild(tagIcon)
+
+	const filterInput = document.getElementById(`input_${selectedFilter}`)
+	filterInput.value = ''
+	const filterListInput = document.getElementById(`filter_by_${selectedFilter}`)
+	filterListInput.style.display = 'none'
+	const filterList = document.getElementById(`filter_list_${selectedFilter}`)
+	filterList.style.display = 'none'
+	const filterButton = document.getElementById(`filter_btn_${selectedFilter}`)
+	filterButton.style.display = 'block'
+
+	// ajoute le tag dans tagList
+	tagList.push(e.target.innerText)
+	// concatene les 2 tableaux de keyword
+	globalKeyword = searchBarKeyword.concat(tagList)
+	filterByKeyword(recipes, globalKeyword)
+}
+
+function deleteTag(e) {
+	e.target.parentElement.remove()
+	const tagDiv = document.getElementById('filters_tags')
+	if (tagDiv.innerHTML === '') {
+		tagDiv.classList.remove('filters_tags_active')
+	}
+
+	// supprime le tag de tagList
+	const index = tagList.indexOf(e.target.previousSibling.innerHTML)
+	if (index > -1) {
+		tagList.splice(index, 1)
+	}
+
+	// supprime le tag de globalKeyword
+	const index2 = globalKeyword.indexOf(e.target.previousSibling.innerHTML)
+	if (index2 > -1) {
+		globalKeyword.splice(index2, 1)
+	}
+
+	filterByKeyword(recipes, globalKeyword)
+}
+
+function filterByKeyword(recipes, globalKeyword) {
+	if (globalKeyword.length != 0) {
+		filteredRecipesByTag = recipes
+		// filtre les recettes avec chaque keyword
+		globalKeyword.forEach((keyword) => {
+			filteredRecipesByTag = filterRecipes(keyword, filteredRecipesByTag)
+			// affiche le nombre de recettes filtrées
+			recipesLength.textContent = filteredRecipesByTag.length
+			// affiche les recettes filtrées
+			displayRecipes(filteredRecipesByTag)
+			// réinitialise les listes ingr, app, ustens avec les recettes filtrées
+			listInit(filteredRecipesByTag)
+			// affiche les listes des recettes filtrées
+			displayFilterList(lists)
+		})
+
+		document.querySelectorAll('.filter_li').forEach((li) => {
+			globalKeyword.forEach((keyword) => {
+				if (keyword == li.textContent) {
+					li.classList.add('selected')
+				}
+			})
+		})
+	} else {
+		init()
+		document.querySelector('.search_recipe_number').textContent = '50'
+	}
 }
--- scripts/utils/tag.js
+++ scripts/utils/tag.js
@@ -1,140 +1,73 @@
-let tagList = []
+// import { searchBarKeyword } from '../index.js'
+// import { filterRecipes } from './filterAlgo.js'
 
-export function createTag(e, selectedFilter) {
-	const filterTag = document.getElementById('filters_tags')
-	filterTag.classList.add('filters_tags_active')
+// let tagList = []
+// let globalKeyword = []
+// let filteredRecipesByTag = []
 
-	const tagDiv = document.createElement('div')
-	tagDiv.classList.add('filter_tag_div')
-	tagDiv.classList.add(`color_tag_${selectedFilter}`)
-	filterTag.appendChild(tagDiv)
-
-	const tag = document.createElement('p')
-	tag.classList.add('filter_tag_p')
-	tag.textContent = e.target.innerText
-	tagDiv.appendChild(tag)
-
-	const tagIcon = document.createElement('i')
-	tagIcon.classList.add('far', 'fa-times-circle')
-	tagIcon.addEventListener('click', (e) => deleteTag(e))
-	tagDiv.appendChild(tagIcon)
-
-	const filterInput = document.getElementById(`input_${selectedFilter}`)
-	filterInput.value = ''
-	const filterListInput = document.getElementById(`filter_by_${selectedFilter}`)
-	filterListInput.style.display = 'none'
-	const filterList = document.getElementById(`filter_list_${selectedFilter}`)
-	filterList.style.display = 'none'
-	const filterButton = document.getElementById(`filter_btn_${selectedFilter}`)
-	filterButton.style.display = 'block'
-
-	tagList.push(e.target.innerText)
-	console.log(tagList)
-	return tagList
-}
-
-function deleteTag(e) {
-	e.target.parentElement.remove()
-	const tagDiv = document.getElementById('filters_tags')
-	if (tagDiv.innerHTML === '') {
-		tagDiv.classList.remove('filters_tags_active')
-	}
-	const index = tagList.indexOf(e.target.previousSibling.innerHTML)
-	if (index > -1) {
-		tagList.splice(index, 1)
-	}
-	console.log(tagList)
-	return tagList
-}
-
-// export function filterByTag(e) {
-//   createTag(e.target.innerText);
-//   filledListFilter();
+// export function filterByTag(e, selectedFilter, recipes) {
+// 	createTag(e, selectedFilter)
+// 	filterByKeyword(recipes, globalKeyword)
 // }
 
-// function handleTag() {
-//   filterByTag(e);
-//   filterDatas(e.target.innerText, datas);
+// export function createTag(e, selectedFilter) {
+// 	const filterTag = document.getElementById('filters_tags')
+// 	filterTag.classList.add('filters_tags_active')
+
+// 	const tagDiv = document.createElement('div')
+// 	tagDiv.classList.add('filter_tag_div')
+// 	tagDiv.classList.add(`color_tag_${selectedFilter}`)
+// 	filterTag.appendChild(tagDiv)
+
+// 	const tag = document.createElement('p')
+// 	tag.classList.add('filter_tag_p')
+// 	tag.textContent = e.target.innerText
+// 	tagDiv.appendChild(tag)
+
+// 	const tagIcon = document.createElement('i')
+// 	tagIcon.classList.add('far', 'fa-times-circle')
+// 	tagIcon.addEventListener('click', (e) => deleteTag(e))
+// 	tagDiv.appendChild(tagIcon)
+
+// 	const filterInput = document.getElementById(`input_${selectedFilter}`)
+// 	filterInput.value = ''
+// 	const filterListInput = document.getElementById(`filter_by_${selectedFilter}`)
+// 	filterListInput.style.display = 'none'
+// 	const filterList = document.getElementById(`filter_list_${selectedFilter}`)
+// 	filterList.style.display = 'none'
+// 	const filterButton = document.getElementById(`filter_btn_${selectedFilter}`)
+// 	filterButton.style.display = 'block'
+
+// 	// ajoute le tag dans tagList
+// 	tagList.push(e.target.innerText)
+// 	console.log('tagList', tagList)
+// 	// concatene les 2 tableaux de keyword
+// 	globalKeyword = searchBarKeyword.concat(tagList)
+// 	console.log('globalKeyword', globalKeyword)
+
+// 	return tagList
 // }
 
-// function handleTag(e) {
-// 	const inputSearchBar = document.getElementById('search_recipe')
-// 	//   const tagSection = document.querySelector(".filters_tags_active");
-// 	const tag = e.target.innerText
-// 	createTag(tag)
-// 	if (inputSearchBar.className === 'active') {
-// 		filterDatas(tag, filteredDatas)
-// 		displayRecipe(fileredDatasWithTag)
-// 		filledListFilter(fileredDatasWithTag)
+// function deleteTag(e) {
+// 	e.target.parentElement.remove()
+// 	const tagDiv = document.getElementById('filters_tags')
+// 	if (tagDiv.innerHTML === '') {
+// 		tagDiv.classList.remove('filters_tags_active')
 // 	}
-// 	//   } else if (inputSearchBar.className === "active" && tagSection) {
-// 	//     filterDatas(tag, filteredDatas);
-// 	//   } else {
-// 	//     filterDatas(tag, filteredDatas);
-// 	//   }
+
+// 	// supprime le tag de tagList
+// 	const index = tagList.indexOf(e.target.previousSibling.innerHTML)
+// 	if (index > -1) {
+// 		tagList.splice(index, 1)
+// 	}
+// 	console.log(tagList)
+
+// 	// supprime le tag de globalKeyword
+// 	const index2 = globalKeyword.indexOf(e.target.previousSibling.innerHTML)
+// 	if (index2 > -1) {
+// 		globalKeyword.splice(index2, 1)
+// 	}
+// 	console.log(globalKeyword)
+
+// 	return tagList
 // }
-
-// const tagSection = document.querySelector(".filters_tags_active");
-// if (tagSection) {
-//   const tags = document.querySelectorAll(".filter_tag_p");
-//   let tagList = [];
-//   const tagContent = tags.forEach((tag) => tagList.push(tag.textContent));
-//   debugger;
-//   const newOptionArray = optionArray.filter(
-//     (option) => !option.toString().includes(searchValue)
-//   );
-//   console.log(newOptionArray);
-//   liSection.innerHTML = "";
-//   newOptionArray.forEach((elt) => {
-//     const liFilter = document.createElement("li");
-//     liFilter.classList.add("filter_li");
-//     const eltFormated = majFirstLetter(elt);
-//     liFilter.textContent = eltFormated;
-//     liFilter.addEventListener("click", (e) => handleTag(e));
-//     liSection.appendChild(liFilter);
-//   });
-
-// quand on sélectionne/clique sur filtre
-// on créé un tag html
-// createTag();
-// //**s'il y a déjà eu une recherche dans la searchbar **/
-// // que searchbar est active
-// searchbar.className == "active";
-// // on va filtrer les filteredDatas avec le keywordTag
-// filterDatas(tag, filteredDatas);
-// // puis displayRecipe avec fileredDatasWithTag qui sont le return filteredDatas
-// displayRecipe(fileredDatasWithTag);
-// // on va filledListFilter avec filteredDataWithTag
-// filledListFilter(fileredDatasWithTag);
-// // si on sélectionne/clique sur autre filtre
-// // on créé un tag html
-// createTag();
-// // on va filtrer les filteredDataWithTag avec le keywordTag
-// filterDatas(tag, fileredDatasWithTag);
-// // puis displayRecipe avec fileredDatasWithTag qui sont le return filteredDatas
-// displayRecipe(fileredDatasWithTag);
-// // etc...
-// // si on delete un tag
-
-// //** s'il n'y a pas eu de recherche dans la searchbar */
-// // que filteredDatas est vide
-// filteredDatas === empty;
-// // on va filtrer les datas avec le keywordTag
-// filterDatas(tag, datas);
-// // puis displayRecipe avec fileredDatasWithTag qui sont le return filteredDatas
-// displayRecipe(fileredDatasWithTag);
-// // si on sélectionne/clique sur autre filtre
-// // on créé un tag html
-// createTag();
-// // on va filtrer les filteredDataWithTag avec le keywordTag qui sont le return filteredDatas
-// filterDatas(tag, fileredDatasWithTag);
-// // puis displayRecipe avec fileredDatasWithTag qui sont le return filteredDatas
-// displayRecipe(fileredDatasWithTag);
-// // etc...
-// // si on désélectionne/clique un filtre
-// // on delete un tag html
-// deleteTag();
-// // si input searchbar
-// console.log(
-//   "Veuillez déselectionner le/les filtre(s) pour saisir une recherche"
-// );
